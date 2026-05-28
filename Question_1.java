ab432fa3fb3d8eca48694d09c02316b7dbc78351
Lucky <lucky@localhost>
2026-05-28 19:52:01 +0530

    changed language to java

---
+++ Question_1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Question_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        
+        int sum = 0;
+        // Loop from 1 to N and add each value to 'sum'
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        
+        System.out.println("Sum = " + sum);
+        sc.close();
+    }
+}
