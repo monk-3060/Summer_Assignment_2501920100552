320f81f19a34fcf97d390461117e3c2634c3c974
Lucky <lucky@localhost>
2026-06-11 11:26:20 +0530

    Day_7 completed

---
+++ Day_7/Question_4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Question_4 {
+
+    static void reverse(int n) {
+        if (n == 0)
+            return;
+
+        System.out.print(n % 10);
+        reverse(n / 10);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        reverse(n);
+
+        sc.close();
+    }
+}
