9b9c0492eed2828e0fa5ce4de2861aee5db91cb2
Lucky <lucky@localhost>
2026-06-11 11:30:36 +0530

    Day_8 completed

---
+++ Day_8/Question_3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Question_3 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            for (char ch = 'A'; ch < 'A' + i; ch++) {
+                System.out.print(ch);
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
